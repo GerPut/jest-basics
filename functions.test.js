439f92536d792003f5d4aafc28ec58c80daa2176
Gerhard <gerhard.putter@yahoo.com>
2021-11-02 07:45:07 +0200

    Updated.

--- functions.test.js
+++ functions.test.js
@@ -30,3 +30,27 @@ test('Should be falsy', () => {
 test('User should be Gerhard Putter object', () => {
     expect(functions.createUser()).toEqual({ firstName: 'Gerhard', lastName: 'Putter' });
 })
+
+// Less than and greater than
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThan(1600)
+});
+
+test('Should be under 1600', () => {
+    const load1 = 800;
+    const load2 = 700;
+    expect(load1 + load2).toBeLessThanOrEqual(1600)
+});
+
+//Regex
+test('There is no I in team', () => {
+    expect('team').not.toMatch(/I/)
+})
+
+//Arrays
+test('Admin should be in username', () => {
+    usernames = ['john', 'karen', 'admin']
+    expect(usernames).toContain('admin')
+})
